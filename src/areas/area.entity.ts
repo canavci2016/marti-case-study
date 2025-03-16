867e31cff7130fd222e29860e9bcfc636c52c0ba
can avci <c.avci@astoundcommerce.com>
2025-03-16 18:17:10 +0300

    TASK-0000 area module has been edited

--- src/areas/area.entity.ts
+++ src/areas/area.entity.ts
@@ -1,11 +1,4 @@
-import { User } from 'src/users/user.entity';
-import {
-  Entity,
-  Column,
-  PrimaryGeneratedColumn,
-  OneToOne,
-  JoinColumn,
-} from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Area {
